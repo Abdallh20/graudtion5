ec89ffa69f63ae3fb80d604ba46219dadf62573a
Abdallh20 <abdallhshref4@gmail.com>
2024-11-03 23:22:59 +0200

    message

--- myapi/settings.py
+++ myapi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4wf7rclc2j+n+=cfa4p1s*sp!r^#=ul3nf0v=9!p6@#b7-jhaw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
